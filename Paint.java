6fa6c812287f1e321913d57db0f082d8b6e87fdb
jeevanagi99 <89789326+jeevanagi99@users.noreply.github.com>
2021-09-21 18:17:37 +0530

    Add files via upload

---
+++ Paint.java
@@ -0,0 +1,18 @@
+package com.X_workz.oops;
+
+public class Paint {
+int price;
+String type;
+int quantity;
+String color;
+public void paint() {
+	System.out.println("paint applied to building");
+}
+public void paintcolor() {
+	System.out.println("red color applied to building");
+}
+public void paintType() {
+	System.out.println("waterproof paint applied");
+}
+	}
+
---
+++ Pen.java
@@ -0,0 +1,18 @@
+package com.X_workz.oops;
+
+public class Pen {
+int price;
+String brand;
+String color;
+String type;
+public void writing() {
+	System.out.println("pen used for writing");
+}
+public void smoothly() {
+	System.out.println("Ball-Point pen writes smoothly ");
+}
+public void color() {
+	System.out.println("pen color is red");
+	}
+
+}
